c80aebcda9a78f2d7a5fccf416b14ec66f55329a
Shaila Nasrin <drshaila2021@gmail.com>
2022-05-23 16:38:17 -0500

    post purchase data

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
+// const jwt = require("jsonwebtoken");
 
 // middlewire
 app.use(cors());
@@ -20,13 +22,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const toolCollection = client.db("tool_manufacturer").collections("tools");
+
+    const toolCollection = client.db("tool_manufacturer").collection("tools");
+    const purchaseCollection = client
+      .db("tool_manufacturer")
+      .collection("toolPurchased");
 
     app.get("/tool", async (req, res) => {
       const query = {};
       const cursor = toolCollection.find(query);
       const tools = await cursor.toArray();
       res.send(tools);
+    });
+
+    //get one tool
+    app.get("/tool/:toolId", async (req, res) => {
+      const id = req.params.toolId;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const tool = await toolCollection.findOne(query);
+      res.send(tool);
+    });
+
+    // post purchase data
+    app.post("/purchase", async (req, res) => {
+      const toolPurchased = req.body;
+      const result = await purchaseCollection.insertOne(toolPurchased);
+      res.send(result);
+    });
+    //    get all purchased item for one user
+    app.get("/purchase", async (req, res) => {
+      const email = req.query.user;
+      const query = { email: email };
+      const orders = await purchaseCollection.find(query).toArray();
+      res.send(orders);
     });
   } finally {
   }
